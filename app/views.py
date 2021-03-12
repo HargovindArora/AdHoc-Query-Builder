9ffa775bc0bfbf972fda73f421983b049d512c88
HargovindArora <hargovinds50@gmail.com>
2021-03-12 13:42:12 +0530

    Added route to get all the tables inside a database

--- app/views.py
+++ app/views.py
@@ -5,4 +5,21 @@ from flask import render_template, request, redirect, jsonify, make_response
 
 @app.route("/")
 def index():
-    return render_template("public/index.html")
+
+    SQL = "show tables;"
+    # conn = mysql.connection
+    # cur = conn.cursor()
+    # cur.execute(SQL)
+    # output = cur.fetchall()
+    # print(output)
+
+    df = mysql.execute_sql(SQL, to_pandas=True)
+    tables = {}
+    for col in df.columns:
+        tables[col] = []
+        for x in df[col]:
+            tables[col].append(x)
+
+    print(tables)
+
+    return render_template("public/index.html", tables=tables)
