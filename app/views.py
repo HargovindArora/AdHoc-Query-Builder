a9dcce6e53e89af10202e704dd686c3d6633b756
HargovindArora <hargovinds50@gmail.com>
2021-03-14 13:44:11 +0530

    Created endpoints to select table and columns

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from app import mysql
 
 from flask import render_template, request, redirect, jsonify, make_response
 
-@app.route("/")
-def index():
+query = {}
+
+@app.route("/get_tables")
+def get_tables():
 
     SQL = "show tables;"
     # conn = mysql.connection
@@ -14,12 +16,71 @@ def index():
     # print(output)
 
     df = mysql.execute_sql(SQL, to_pandas=True)
-    tables = {}
+    tables = []
+
     for col in df.columns:
-        tables[col] = []
         for x in df[col]:
-            tables[col].append(x)
+            tables.append(x)
 
-    print(tables)
+    res = make_response(jsonify({"tables": tables}), 200)
 
-    return render_template("public/index.html", tables=tables)
+    return res
+
+
+@app.route("/table", methods=["POST"])
+def table():
+
+    req = request.get_json()
+    table = req['table']
+
+    SQL = f"desc {table};"
+    df = mysql.execute_sql(SQL, to_pandas=True)
+
+    query["table"] = table
+    print(query['table'])
+
+
+    cols = []
+
+    for x in df['Field']:
+        cols.append(x)
+
+    res = make_response(jsonify({"columns": cols}), 200)
+
+    return res
+
+# print(query)
+
+
+@app.route("/column", methods=["POST"])
+def column():
+
+    req = request.get_json()
+    column = req['column']
+
+    try:
+        SQL = f"desc {query['table']};"
+        df = mysql.execute_sql(SQL, to_pandas=True)
+    except KeyError:
+        return "No Table Found!", 200
+
+    cols = []
+
+    for x in df['Field']:
+        cols.append(x)
+
+    if not column or column[0]=="all" or len(column)==len(cols):
+        query["column"] = "*"
+    else:
+        if len(column) == 1:
+            query["column"] = column[0]
+        else:
+            query["column"] = []
+            for col in column:
+                query["column"].append(col)
+
+    print(query)
+
+    
+    
+    return 'Done'
